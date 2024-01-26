1b91f3be4b3477732e94543b0eb74d614d9ec5e1
petermg <pgomberg@gmail.com>
2024-01-25 18:31:27 -0800

    Update depth_map_to_stl,_2.py
    Modified to run locally.

--- depth_map_to_stl,_2.py
+++ depth_map_to_stl,_2.py
@@ -18,53 +18,46 @@ Project is at https://github.com/BillFSmith/depth_map_to_stl
 
 Example depth map from https://johnflower.org/sites/default/files/2018-07/08-new-plymouth-15m-dem-n-mt-taranaki_5.png
 """
-
 #@title Press Ctrl + F9 to run code
-
 print("Depth maps only show relative depth, so the division of the maximum depth and the width of the image is needed.")
 print("E.g. an object that is 10cm tall and 20cm wide, the value would be 0.5")
 height_div_width = input('Enter desired value for image depth/width: ')
 print('')
-print("Upload depth maps:")
+print("--Drag and Drop Depth map Below--")
 
-from google.colab import files
-uploaded = files.upload()
-
-!pip install numpy-stl
+#from google.colab import files
+file = input("Drag file HERE:-->")
+print('the following file is going to be processed:' + ' ' + file)
+#uploaded = files.upload()
+#!pip install numpy-stl
 import numpy as np
 from stl import mesh
 import cv2
-
-file_names = list(uploaded.keys())
-
-for file_name in file_names:
-    im = cv2.imread(file_name, cv2.IMREAD_UNCHANGED)
-    im_array = np.array(im) #.transpose((1, 0, 2))
-    im_array = np.rot90(im_array, -1, (0,1))
-
-    mesh_size = [im_array.shape[0],im_array.shape[1]]
-
-    mesh_max = np.max(im_array)
-
-    if len(im_array.shape) == 3:
+import sys
+im = cv2.imread(file, cv2.IMREAD_UNCHANGED)
+im_array = np.array(im) #.transpose((1, 0, 2))
+im_array = np.rot90(im_array, -1, (0,1))
+mesh_size = [im_array.shape[0],im_array.shape[1]]
+mesh_max = np.max(im_array)
+if len(im_array.shape) == 3:
         scaled_mesh = mesh_size[0] * float(height_div_width) * im_array[:,:,0] / mesh_max
-    else:
+else:
         scaled_mesh = mesh_size[0] * float(height_div_width) * im_array / mesh_max
     # rand_mesh = np.random.rand(mesh_size[0],mesh_size[1])
 
-    mesh_shape = mesh.Mesh(np.zeros((mesh_size[0] - 1) * (mesh_size[1] - 1) * 2, dtype=mesh.Mesh.dtype))
+mesh_shape = mesh.Mesh(np.zeros((mesh_size[0] - 1) * (mesh_size[1] - 1) * 2, dtype=mesh.Mesh.dtype))
 
-    for i in range(0, mesh_size[0]-1):
+for i in range(0, mesh_size[0]-1):
         for j in range(0, mesh_size[1]-1):
             mesh_num = i * (mesh_size[1]-1) + j
-
             mesh_shape.vectors[2 * mesh_num][2] = [i, j, scaled_mesh[i,j]]
             mesh_shape.vectors[2 * mesh_num][1] = [i, j+1, scaled_mesh[i,j+1]]
             mesh_shape.vectors[2 * mesh_num][0] = [i+1, j, scaled_mesh[i+1,j]]
-
             mesh_shape.vectors[2 * mesh_num + 1][0] = [i+1, j+1, scaled_mesh[i+1,j+1]]
             mesh_shape.vectors[2 * mesh_num + 1][1] = [i, j+1, scaled_mesh[i,j+1]]
             mesh_shape.vectors[2 * mesh_num + 1][2] = [i+1, j, scaled_mesh[i+1,j]]
 
-    mesh_shape.save(str(file_name) + '.stl')
-    files.download(str(file_name) + '.stl')
+mesh_shape.save((file) + '.stl')
+print('done')
+sys.exit()
+#files((file_name) + '.stl')
